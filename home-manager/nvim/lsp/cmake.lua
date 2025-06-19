28ad9e665ff1a07cf15ceade5e576079975eca06
gen740 <54583542+gen740@users.noreply.github.com>
2025-06-19 22:55:23 +0900

    feat(lsp): Update lsp settings

--- home-manager/nvim/lsp/cmake.lua
+++ home-manager/nvim/lsp/cmake.lua
@@ -1,7 +1,23 @@
 return {
   cmd = {
-    'cmake-language-server',
+    'nix', 'run', 'nixpkgs#cmake-language-server',
   },
-  root_markers = { 'CMakeLists.txt' },
   filetypes = { 'cmake' },
+  init_options = {
+    buildDirectory = 'build',
+  },
+  single_file_support = true,
+  on_attach = function(client, bufnr)
+    local chars = {}
+    for i = 32, 126 do
+      table.insert(chars, string.char(i))
+    end
+    client.server_capabilities.completionProvider.triggerCharacters = chars
+    vim.lsp.completion.enable(true, client.id, bufnr, {
+      autotrigger = true,
+      convert = function(item)
+        return { abbr = item.label:gsub('%b()', '') }
+      end,
+    })
+  end,
 }
