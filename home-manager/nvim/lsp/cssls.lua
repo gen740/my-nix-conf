af9472627febf6dc7370c1ab15b17f073f14e826
gen740 <54583542+gen740@users.noreply.github.com>
2025-08-24 13:17:28 +0900

    feat(prittier): Add prettier to format CSS/HTML

--- home-manager/nvim/lsp/cssls.lua
+++ home-manager/nvim/lsp/cssls.lua
@@ -1,13 +1,42 @@
 return {
   cmd = {
     'nix',
-    'run',
-    'nixpkgs#vscode-css-languageserver',
-    '--',
+    'shell',
+    'nixpkgs#vscode-langservers-extracted',
+    '-c',
+    'vscode-css-language-server',
     '--stdio',
   },
   filetypes = { 'css' },
-  on_attach = function(client, bufnr)
-    client.server_capabilities.document_formatting = false
-  end,
+  -- settings = {
+  --   css = {
+  --     validate = true,
+  --     lint = {
+  --       unknownAtRules = "ignore",
+  --       unknownProperties = "ignore",
+  --       validProperties = {},
+  --     },
+  --     completion = {
+  --       completePropertyWithSemicolon = true,
+  --       triggerPropertyValueCompletion = true
+  --     }
+  --   },
+  --   scss = {
+  --     validate = true,
+  --     lint = {
+  --       unknownAtRules = "ignore",
+  --       unknownProperties = "ignore"
+  --     }
+  --   },
+  --   less = {
+  --     validate = true,
+  --     lint = {
+  --       unknownAtRules = "ignore",
+  --       unknownProperties = "ignore"
+  --     }
+  --   }
+  -- },
+  -- on_attach = function(client, _)
+  --   client.server_capabilities.document_formatting = false
+  -- end,
 }
