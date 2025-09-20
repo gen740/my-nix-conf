c29e5fa2faa490d4637eced8f5c5a9ca2db74c14
gen740 <54583542+gen740@users.noreply.github.com>
2025-09-20 22:47:16 +0900

    feat(darwin): Add jankyborders

--- hardwares/darwin/configuration.nix
+++ hardwares/darwin/configuration.nix
@@ -43,6 +43,14 @@
     optimise.automatic = true;
   };
 
+  services.jankyborders = {
+    active_color = "0xFFFFFF00";
+    background_color = "";
+    enable = true;
+    hidpi = true;
+    inactive_color = "0x00000000";
+  };
+
   fonts.packages = with pkgs; [
     nerd-fonts.fira-code
   ];
